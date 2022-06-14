538ea7af8a7431e23fdd9cb6748204bcd128da17
Rafael Pereira Dias <br.rafaeldias@gmail.com>
2022-06-14 16:31:58 -0300

    update tutorial part8
    tutorial completed fully

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -6,15 +6,30 @@ namespace RazorPagesMovieMicrosoftTutorial.Models
     public class Movie
     {
         public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; } = string.Empty;
 
         [Display(Name = "Data de Lançamento")]
         [DataType(DataType.Date)]
+        [Required]
         public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; } = string.Empty;
 
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18,2)")]
+        [Required]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
         public string Rating { get; set; } = string.Empty;
     }
 }
