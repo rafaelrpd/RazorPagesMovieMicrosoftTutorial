d474d0439309c0c4c844fe751533b2bbd0d1df11
Rafael Pereira Dias <br.rafaeldias@gmail.com>
2022-06-14 14:09:36 -0300

    update part7
    part7 done with sucess and no db errors

--- Migrations/20220614170106_Rating.Designer.cs
+++ Migrations/20220614170106_Rating.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorPagesMovieMicrosoftTutorial.Data;
 
@@ -11,9 +12,10 @@ using RazorPagesMovieMicrosoftTutorial.Data;
 namespace RazorPagesMovieMicrosoftTutorial.Migrations
 {
     [DbContext(typeof(RazorPagesMovieMicrosoftTutorialContext))]
-    partial class RazorPagesMovieMicrosoftTutorialContextModelSnapshot : ModelSnapshot
+    [Migration("20220614170106_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
